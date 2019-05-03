c08e44efde25917ea2a7da99491e9a848cd9cf2d
rfenouil <35697321+rfenouil@users.noreply.github.com>
2019-05-04 01:49:56 +0200

    Country 00 (#1)
    * update_19042019 | New and improved script that fixes execution errors
    
    * Fix libraries install and copy of regulatory.bin
    
    User on arm device Rock64 with linux kernel 4.4.167 (4.4.167-1169-rockchip-ayufan-g3cde5c624c9c).
    
    I had to make these change to get the script to work:
    Removed libraries from apt install command conflicted with installed version and made the script fail
    I had no pre-existing folder `/lib/crda` so it had to be created (with `/lib/crda/pubkeys`) before copying files
    Added extra test to prevent overwriting backup bin
    
    My apologies if that breaks something on for others, feel free to correct or adapt.
    
    * Update wifi_txpower_unlocker.sh
    
    Alter country 00 which is default selection on boot instead of country BO that requires `iw reg set BO` after boot.

---
+++ wifi_txpower_unlocker.sh
@@ -0,0 +1,73 @@
+#!/bin/bash -e
+
+#title: wifi_txpower_unlocker.sh
+#description: Unlocks the wifi txpower of the 2.4Ghz band of the BO region 
+#author: Hiruna Wijesinghe https://github.com/hiruna/wifi-txpower-unlocker/
+#date: 19/04/2019
+
+# Input the desired transmit power (dBm) (I used 33dBm for 2W)
+txpower="$1"
+
+if [ -z $txpower ]; then
+	echo "TX power not specified. Exitting..." 1>&2
+	exit 1
+fi
+
+if ! [[ "$txpower" =~ ^[0-9]+$ ]]; then
+    echo "TX power input must be an integer. Exitting..."
+	exit 1
+fi
+
+# APT update & upgrade
+echo "Update/Upgrade packages..." 1>&2
+apt-get -y update && apt-get -y upgrade
+
+# Install dependencies
+echo "Installing dependencies..." 1>&2
+apt-get -y install git python m2crypto pkg-config build-essential python-dev python-pip curl
+pip install setuptools future
+
+echo "Fetching crda and wireless-regdb..." 1>&2
+# Clone crda
+git clone git://git.kernel.org/pub/scm/linux/kernel/git/mcgrof/crda.git
+
+# Clone wireless-regdb
+git clone git://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git
+
+# Change region 'BO's TXpower in db.txt
+echo "Setting the desired TX power for 'country 00' in wireless-regdb/db.txt ..." 1>&2
+gawk -i inplace -v INPLACE_SUFFIX=.bak -F"[ ]" '$1~"country"{if($2~"00:") countryOK=1; else countryOK=0;} ( countryOK==1 && $6~/^\(/ ){ sub(/[0-9]+/, "33", $6);} {}1' wireless-regdb/db.txt
+
+# Compile wireless-regdb (regulatory.bin)
+echo "Compiling wireless-regdb (regulatory.bin)..." 1>&2
+make -C wireless-regdb/
+
+# Backup old regulatory.bin and copy the newly compiled file into /lib/crda/
+echo "Backing up old regulatory.bin and copying the newly compiled file into /lib/crda/ ..." 1>&2
+mkdir -p /lib/crda/pubkeys
+if [[ -f /lib/crda/regulatory.bin ]] && [[ ! -f /lib/crda/regulatory.bin.old ]]; then
+	cp /lib/crda/regulatory.bin /lib/crda/regulatory.bin.old
+fi
+cp wireless-regdb/regulatory.bin /lib/crda/
+
+# Newly compiled regulatory.bin was signed by the current user, copy this key into crda
+echo "Copying key signed by $(whoami) for the new regulatory.bin into crda..." 1>&2
+cp wireless-regdb/$(whoami).key.pub.pem crda/pubkeys/
+cp wireless-regdb/$(whoami).key.pub.pem /lib/crda/pubkeys/
+
+# Change regulatory.bin path in the crda/Makefile to /lib/crda/regulatory.bin
+echo "Changing regulatory.bin path in the crda/Makefile to /lib/crda/regulatory.bin ..." 1>&2
+sed -i '/REG_BIN?=\/usr\/lib\/crda\/regulatory.bin/!b;cREG_BIN?=\/lib\/crda\/regulatory.bin' crda/Makefile
+
+# Remove -Werror option when compiling
+echo "Removing -Werror option when compiling..." 1>&2
+sed -i '/CFLAGS += -std=gnu99 -Wall -Werror -pedantic/!b;cCFLAGS += -std=gnu99 -Wall -pedantic' crda/Makefile
+
+# Clean and compile
+echo "Clean and compile crda..." 1>&2
+make clean -C crda/
+make -C crda/
+make install -C crda/
+
+# Reboot to apply changes
+printf 'A system reboot is required to apply the changes. Do you want to reboot now ? (y/n)' && read reboot_prompt && [[ "$reboot_prompt" == "y" ]] && reboot;
